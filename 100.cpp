9b2a61e6c009721a8d0b692e94e00f5af98b4c93
ed@ed.com <ed@ed.com>
2013-05-18 18:37:58 -0400

    improved cache works

--- 100.cpp
+++ 100.cpp
@@ -4,6 +4,8 @@
 //recursive solution             .605
 //unoptimized cache              .309
 //optimized cache                .216
+//slightly more optimized        .225  (avoids ==1 check)
+//avoids any rebucketing         .169
 
 #include <iostream>
 #include <ext/hash_map>
@@ -11,30 +13,28 @@
 using namespace std;
 using namespace __gnu_cxx;
 
-typedef hash_map<unsigned int, int> cache_t;
+typedef hash_map<unsigned int, short> cache_t;
 cache_t cache;
 
 int cycleLengthRecursive(unsigned int n) {
-
- 
-  if (n == 1) return 1;
+  // base case is handled by cache
   
   cache_t::iterator it = cache.find(n);
   if (it != cache.end()) {
     return it->second;  
   }
   
-  
-  unsigned int n1;
-  if (n % 2 == 1) n1 = (n*3) + 1;  // if n is odd then  n <- 3n + 1
-  else            n1 = n/2;        // else n <- n/2
+  int out;
+  if (n % 2 == 1) 
+    out = 1 + cycleLengthRecursive((n*3) + 1);  // if n is odd then  n <- 3n + 1
+  else           
+    out = 1 + cycleLengthRecursive(n/2);        // else n <- n/2
 
-  int out = 1 + cycleLengthRecursive(n1);
   cache[n] = out;
   return out;
 }
 
-int maxCycleLength(int i, int j) {
+int maxCycleLength(unsigned int i, unsigned int j) {
   
   if (j<i) { // arange them in min/max order
     int tmp = i;
@@ -42,7 +42,6 @@ int maxCycleLength(int i, int j) {
     j = tmp;
   }
 
-  //loop from min to small and keep track of max
   int max = 0;
   for (int n=i; n<=j; ++n){
     int c = cycleLengthRecursive(n);
@@ -53,7 +52,9 @@ int maxCycleLength(int i, int j) {
 }
 
 int main(int argc,char *argv[]) {
-    int i, j;
+    cache.resize(3145739); // avoid any future rebucketing
+    cache[1] = 1; // base case, avoids lots of future ==
+    unsigned int i, j;
     while (cin >> i && cin >> j) {
       cout << i << " " << j << " " << maxCycleLength(i, j) << endl;
     }
